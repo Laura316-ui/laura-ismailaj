55682bcfe6ddbc6a0d888ae6aee2a80492e8ee0e
Laura316-ui <lauraismailaj3@gmail.com>
2025-09-26 09:02:22 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,3 +1,46 @@
+FAZA 2
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World");
+    }
+}
+FAZA 3
+import java.util.*;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class Main {
+    public static void main(String[] args) {
+        // Printo mesazhin e parë
+        System.out.println("Hello, World");
+        
+        // Printo përshëndetjen me emrin tënd
+        System.out.println("Përshëndetje, Kristi!");
+        
+        // Merr datën dhe orën aktuale
+        LocalDateTime now = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+        String dateTime = now.format(formatter);
+        
+        // Printo datën dhe orën
+        System.out.println("Sot është: " + dateTime);
+        
+        // Përcakto përshëndetjen sipas orës
+        int hour = now.getHour();
+        String greeting;
+        if (hour >= 5 && hour < 12) {
+            greeting = "Mirëmëngjes";
+        } else if (hour >= 12 && hour < 18) {
+            greeting = "Mirëdita";
+        } else {
+            greeting = "Mirëmbrëma";
+        }
+        
+        // Printo mesazhin final
+        System.out.println(greeting + " dhe mirë se erdhe në Java!");
+    }
+}
+FAZA 4
 import java.util.Scanner;
 
 public class  {
@@ -13,6 +56,19 @@ public class  {
         System.out.println("Përshëndetje, " + emri + " nga " + qyteti + "!");
 
         scanner.close();
+    }
+}
+FAZA 5
+Kodi fillestar
+public class helloWorld {
+    public static void main(String args) {
+        System.out.println("Hello World";
+    }
+}
+korrigjimi
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
     }
 }
 
